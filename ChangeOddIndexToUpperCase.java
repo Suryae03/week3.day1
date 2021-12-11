7d84077045a0f8ef1194fcee815e939961a35f40
Suryae03 <95089572+Suryae03@users.noreply.github.com>
2021-12-12 03:40:07 +0530

    Add files via upload

---
+++ ChangeOddIndexToUpperCase.java
@@ -0,0 +1,12 @@
+package week3.day1.assignments;
+
+public class ChangeOddIndexToUpperCase {
+
+	public static void main(String[] args) 
+	{
+		String test = "changeme";
+		char[] ch=test.toCharArray();
+
+	}
+
+}
