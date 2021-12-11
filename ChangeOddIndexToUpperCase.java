c79d62d757cad6a3b44b747df700fe74b9ee3552
Suryae03 <95089572+Suryae03@users.noreply.github.com>
2021-12-12 03:48:09 +0530

    Add files via upload

--- ChangeOddIndexToUpperCase.java
+++ ChangeOddIndexToUpperCase.java
@@ -5,7 +5,18 @@ public class ChangeOddIndexToUpperCase {
 	public static void main(String[] args) 
 	{
 		String test = "changeme";
-		char[] ch=test.toCharArray();
+		for(int i=0; i<test.length(); i++)
+		{
+			char ch = test.charAt(i);
+		    if (i % 2 == 0) 
+		    {
+		        System.out.print(ch);
+		    } 
+		    else 
+		    {
+		        System.out.print(Character.toUpperCase(ch));
+		    }
+		}
 
 	}
 
